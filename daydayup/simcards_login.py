fbd65480c3f6698d57c89ca701a055dcfb478ecd
wming2021 <wmddream2013@qq.com>
2021-03-13 21:59:07 +0800

    测试 ｃｏｍｍｉｔ和　ｃｏｍｍｉｔ　ａｎｄ　ｐｕｌｌ的区别

--- daydayup/simcards_login.py
+++ daydayup/simcards_login.py
@@ -2,3 +2,4 @@ from selenium import webdriver
 
 chromedriver = 'C:\Program Files\Google\Chrome\Application\chromedriver.exe'
 browser = webdriver.Chrome(executable_path=chromedriver)
+# 正在试试看  commit 和 commit and pull的区别
