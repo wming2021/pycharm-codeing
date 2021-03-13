95e95029af57919e17ed5e5d84650f348c05a840
wming2021 <wmddream2013@qq.com>
2021-03-13 21:54:31 +0800

    首次提交

---
+++ daydayup/simcards_login.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+
+chromedriver = 'C:\Program Files\Google\Chrome\Application\chromedriver.exe'
+browser = webdriver.Chrome(executable_path=chromedriver)
---
+++ selenium-3-4.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+import time
+
+chromedriver = 'C:\Program Files\Google\Chrome\Application\chromedriver.exe'
+browser = webdriver.Chrome(executable_path=chromedriver)
+
+
+browser.get('https://www.baidu.com')
+input_en = browser.find_element_by_id('kw')
+input_en.send_keys('selenium python 教程')
+time.sleep(2)
+btn_en = browser.find_element_by_id('su')
+btn_en.click()
+time.sleep(10)
+browser.close()
+# 一堆的报错  然后是解决了一个问题  就解决了错误的问题 哈哈哈
+
