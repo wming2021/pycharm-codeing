83063f120b5715c4b7e77dc4ce791043efe112e4
wming2021 <wmddream2013@qq.com>
2021-03-14 02:49:42 +0800

    前一个操作是 只push  然后我发现  好像不行啊 没有在github上面找到更新的文件 我这次又来使用commit and  PUSH
    然后操作的时候出现了 一个问题 我百度尝修复  现在试试看

--- venv/test-git-github.py
+++ venv/test-git-github.py
@@ -9,3 +9,6 @@ if __name__ == '__main__':
     main()
 
 # 这次是只pull  而没有选择commit and  pull 132
+
+# 一次commit and push 的时候 出现错误  OpenSSL SSL_read: Connection was reset, errno 10054
+# 一次commit and push 的时候 出现错误  OpenSSL SSL_read: Connection was reset, errno 10054
